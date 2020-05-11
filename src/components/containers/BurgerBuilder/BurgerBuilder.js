a5b12007d72041f39d53627587df5efadb2c76b7
Sergey Germanov <sergyleo@yahoo.com>
2020-05-11 16:58:26 +1200

    igredients added to array and burger

--- src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import Auxilary from '../../../hoc/Auxilary';
+import Burger from '../../Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
     render() {
         return (
             <div>
                 <Auxilary>
-                    <div>
-                        Burger
-                    </div>
+                    <Burger ingredients={this.state.ingredients} />
                     <div>
                         Build Controls
                     </div>

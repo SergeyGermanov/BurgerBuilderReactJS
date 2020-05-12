8d01319b2a74f03cbff026ca790f9f0bf39a1528
Sergey Germanov <sergyleo@yahoo.com>
2020-05-12 20:05:37 +1200

    resposive menu added

--- src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Auxilary from '../../../hoc/Auxilary';
+import Auxilary from '../../../hoc/Auxilary/Auxilary';
 import Burger from '../../Burger/Burger';
 import BuildControls from '../../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../../components/UI/Modal/Modal';
